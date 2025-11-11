cdeb5845a5326aaf554fcaf83add76e5f940a2e8
Kittisak Phetrungnapha <ktsprnp@gmail.com>
2025-11-11 10:28:32 +0700

    Initial Commit

---
+++ AssetsPriceTracker/AssetsPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsPriceTrackerApp.swift
+//  AssetsPriceTracker
+//
+//  Created by Kittisak Phetrungnapha on 11/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AssetsPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
