7b88d4fdd8b98e7b5374f8ba34d430ab01b82b9a
Kittisak Phetrungnapha <ktsprnp@gmail.com>
2025-11-11 16:05:39 +0700

    Add Asset enum for supported assets
    Introduces the Asset enum in Asset.swift, listing supported stocks and cryptocurrencies with their respective codes. This will be used to represent and encode/decode asset types throughout the app.

---
+++ AssetsPriceTracker/Models/Asset.swift
@@ -0,0 +1,36 @@
+//
+//  Asset.swift
+//  AssetsPriceTracker
+//
+//  Created by Kittisak Phetrungnapha on 11/11/2568 BE.
+//
+
+import Foundation
+
+enum Asset: String, Codable {
+    case apple = "AAPL"
+    case google = "GOOG"
+    case tesla = "TSLA"
+    case amazon = "AMZN"
+    case microsoft = "MSFT"
+    case nvidia = "NVDA"
+    case bitcoin = "BTC"
+    case ethereum = "ETH"
+    case binanceCoin = "BNB"
+    case solana = "SOL"
+    case ripple = "XRP"
+    case cardano = "ADA"
+    case tron = "TRX"
+    case doge = "DOGE"
+    case hyperliquid = "HYPE"
+    case chainlink = "LINK"
+    case bitcoinCash = "BCH"
+    case stellar = "XLM"
+    case litecoin = "LTC"
+    case sui = "SUI"
+    case monero = "XMR"
+    case shiba = "SHIB"
+    case uniswap = "UNI"
+    case ton = "TON"
+    case aave = "AAVE"
+}
